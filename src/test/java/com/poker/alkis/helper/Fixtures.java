27ba5627f1dfcd4f8f1a1bee7ef17283e8974d89
kalogirou <vka@anixe.io>
2025-01-03 09:11:31 +0200

    test fixes

--- src/test/java/com/poker/alkis/helper/Fixtures.java
+++ src/test/java/com/poker/alkis/helper/Fixtures.java
@@ -1,31 +1,32 @@
 package com.poker.alkis.helper;
 
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
-
+import com.poker.alkis.entities.Player;
+import com.poker.alkis.entities.PokerSession;
+import com.poker.alkis.enums.Season;
+import com.poker.alkis.repos.PokerSessionRepo;
+import com.poker.alkis.services.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.poker.alkis.entities.Player;
-import com.poker.alkis.entities.PokerSession;
-import com.poker.alkis.repos.PlayerRepo;
-import com.poker.alkis.repos.PokerSessionRepo;
-import com.poker.alkis.services.PlayerService;
+import java.time.LocalDate;
 
 @Component
 public class Fixtures {
-    
+
     @Autowired
     private PlayerService playerService;
-    
+
     @Autowired
     private PokerSessionRepo pokerSessionRepo;
-    
+
     public PokerSession buildAndSavePokerSession() {
         Player host = playerService.getOrCreatePlayer("host");
-        PokerSession pokerSession = new PokerSession(host, LocalDate.now().plusYears(1), "just a poker session");
-        
+        PokerSession pokerSession = new PokerSession(host,
+                                                     LocalDate.now().plusYears(1),
+                                                     "just a poker session",
+                                                     Season.SEASON_2025_A);
+
         return pokerSessionRepo.saveAndFlush(pokerSession);
     }
-    
+
 }
