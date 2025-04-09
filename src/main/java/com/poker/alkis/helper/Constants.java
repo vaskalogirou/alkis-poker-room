de7d9eb2a6b3cccb0eead8969b4e1271f3f2c8e7
kalogirou <vaskalogirou@gmail.com>
2025-04-10 00:39:25 +0300

    introduce new season constant

--- src/main/java/com/poker/alkis/helper/Constants.java
+++ src/main/java/com/poker/alkis/helper/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 
     public static final String CONTENT_TYPE_APPLICATION_JSON = "application/json;charset=UTF-8";
 
-    public static final Season CURRENT_SEASON = Season.SEASON_2025_A;
+    public static final Season CURRENT_SEASON = Season.SEASON_2025_B;
 
     public static final String SPACE = " ";
 }
